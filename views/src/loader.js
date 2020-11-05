d0d8f52beb7ac7c59b9fa74119c63f3c5a5b8e97
sheikhmishar <misharex.sezan@gmail.com>
2020-11-05 22:34:22 +0600

    moved all static assets to root

--- views/src/loader.js
+++ views/src/loader.js
@@ -7,7 +7,7 @@
 
 import loadScripts from '../lib/asyncScriptsLoader'
 
-const scriptUrls = ['/io/socket.io.js', '/axios.min.js']
+const scriptUrls = ['/socket.io.js', '/axios.min.js']
 const callback = () => import('./js').then()
 
 loadScripts(scriptUrls, callback)
